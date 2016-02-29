528359a9e67b823790fd8f979d45c1e06af01df8
CKiilu <cndeti@gmail.com>
2016-02-29 21:15:34 +0300

    create second screen class and getnamescreenintent

---
+++ app/src/main/java/com/scurrae/chris/switchingscreens/SecondScreen.java
@@ -0,0 +1,14 @@
+package com.scurrae.chris.switchingscreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chris on 2/29/16.
+ */
+public class SecondScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
